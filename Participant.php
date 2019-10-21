a0e0490b01f54af8bafa8f5c246af732fe1d31f4
ASO1701006 <1701006@st.asoujuku.ac.jp>
2019-10-21 12:09:17 +0900

    一応完成品 tableのプルダウンの動きは完成してない(T . T)

--- Participant.php
+++ Participant.php
@@ -19,7 +19,7 @@
                 </th>
             </tr>
             <tr>
-                <td colspan="2" class="colspan">説明会管理
+                <td colspan="2" class="colspan">参加者一覧
                     <a href="List.php">□</a>
                     <a href="Calendar.php">◇</a>
                     <a href="Join.php" style="position: absolute; right: 130px; top: 65px;">新規説明会追加</a>
@@ -30,6 +30,54 @@
         </table>
     </nav>
 </header>
+<body>
+    <table>
+        <tr>
+            <td>1901001</td>
+            <td>麻生情報ビジネス専門学校</td>
+            <td>説明会</td> <!--前の画面から読み込む-->
+        </tr>
+    </table>
+
+    <table align="center">
+        <tr>
+            <td width="100">参加者</td>
+            <td width="100"><!--DB--></td>
+            <td width="150"></td>
+            <td width="450"></td>
+            <td width="500">学生一覧</td>
+
+        </tr>
+    </table>
+
+    <table align="left" width="45%" frame="box">
+        <tr>
+            <td width="80">1701021</td>
+            <td width="80">是末真志</td>
+            <td width="40">男</td>
+            <td width="40">20</td>
+            <td width="280">麻生情報ビジネス専門学校</td>
+            <td width="40">3</td>
+            <td width="40">➖</td>
+        </tr>
+    </table>
+
+    <table align="right" width="45%" frame="box">
+        <tr>
+            <td width="80">1701022</td>
+            <td width="80">小嶋大地</td>
+            <td width="40">男</td>
+            <td width="40">20</td>
+            <td width="280">麻生情報ビジネス専門学校</td>
+            <td width="40">3</td>
+            <td width="40">➕</td>
+        </tr>
+    </table>
+
+<!--  追加・削除　矢印  --><!--ボタンにしてるけど別に矢印自体にはモーションがないから画像でもいい-->
+    <input type="image" src=".idea/Arrowsout.png" style="position: absolute; top: 250px; left: 700px">
+    <input type="image" src=".idea/Arrowsin.png" style="position: absolute; top: 400px; left: 700px">
+</body>
 </html>
 
 
